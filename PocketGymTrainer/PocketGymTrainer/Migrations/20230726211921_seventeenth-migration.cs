677e2ba9b5642e9a8b7d7f2398da47b63d2e04cd
OreoPL-studio <pawel.floryan@interia.pl>
2023-07-26 23:25:49 +0200

    Added new field into Exercise table

---
+++ PocketGymTrainer/PocketGymTrainer/Migrations/20230726211921_seventeenth-migration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PocketGymTrainer.Migrations
+{
+    /// <inheritdoc />
+    public partial class seventeenthmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Exercise",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Exercise");
+        }
+    }
+}
